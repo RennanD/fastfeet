bed9d50e1e90fb402e7f7c9f20058a7adfaf3eb0
RennanD <rplaykl@gmail.com>
2020-02-06 20:57:55 -0300

    feat: Exception handlers

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,29 @@
 import express from 'express';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
 import { resolve } from 'path';
 
+import 'express-async-errors';
+
 import routes from './routes';
+
+import sentrConfig from './config/sentry';
 
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentrConfig);
+
     this.middlewares();
     this.routes();
+    this.execptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -22,6 +33,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  execptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      await new Youch(err, req).toJSON();
+
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
